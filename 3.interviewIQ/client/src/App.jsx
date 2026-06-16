4c4bddb6160c3761db14e527780761ba2bef492c
Tushar Kumar <tusharkrverma224@gmail.com>
2026-06-16 16:04:42 +0530

    Update ServerUrl to production backend

--- 3.interviewIQ/client/src/App.jsx
+++ 3.interviewIQ/client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "http://localhost:8000"
+export const ServerUrl  = "https://three-interviewiq-backend-gd5k.onrender.com"
 
 function App() {
 
